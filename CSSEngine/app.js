76a614ed7dbe29127b380efdbd8cce48d2ee6abd
belbcode <102109283+belbcode@users.noreply.github.com>
2022-08-10 12:23:42 -0700

    Add files via upload

--- CSSEngine/app.js
+++ CSSEngine/app.js
@@ -1,43 +1,69 @@
 const myElement = document.getElementById('mySelector')
-console.log(myElement.style.top)
 
-const addToStyle = (styleToModify, toAdd) => {
-    console.log(styleToModify,parseInt(styleToModify),toAdd)
-    let extVal = parseInt(styleToModify) ? parseInt(styleToModify) : 0
-    console.log(extVal)
-    return `${String(extVal + toAdd)}px`
-}
-
-const updateBoxPos = (event, prevCords) => {
-    console.log(event.x, event.y, myElement.style.top)
-    myElement.style.top = addToStyle(myElement.style.top, 20);
-}
-
-const myEvent = (event) => {
-    console.log('tracking mouse position')
-    updateBoxPos(event)
-}
-
-console.log({myElement})
 
 myElement.addEventListener('mouseover', () => {
     myElement.style.border = '2px solid lightblue'
     myElement.style.cursor = 'pointer'
 })
-myElement.addEventListener('mouseleave', ()=> {
+myElement.addEventListener('mouseleave', () => {
     myElement.style.border = ''
 })
 
-myElement.addEventListener('mousedown', (event)=> {
-    myElement.style.cursor =  'move';
+// const addToStyle = (styleToModify, toAdd) => {
+//     let extVal = parseInt(styleToModify) ? parseInt(styleToModify) : 0
+//     return `${String(extVal + toAdd)}px`
+// }
 
-    myElement.addEventListener('mousemove', myEvent);
+// const updateBoxPos = (event, difference) => {
+//     console.log(event)
+//     myElement.style.top = addToStyle(myElement.style.top, 3);
+// }
+
+// const myEvent = (event) => {
+//     myElement.addEventListener('mousemove', (event) => {
+//         updateBoxPos(event)
+//     })
+// }
+
+// myElement.addEventListener('mousedown', (event) => {
+//     myElement.style.cursor =  'move';
+//     // myEvent(event);
+// })
+
+// myElement.addEventListener('mouseup', (event) => {
+//     myElement.style.cursor = 'pointer'
+
+// })
+let myObj = myElement.style.cssText
+console.log(myObj.cssText)
+
+let shouldMove = false
+
+prevX = null
+prevY = null
+
+myElement.addEventListener('mousemove', (e) => {
+    let x = e.x
+    let y = e.y
+    myElement.addEventListener('mousedown', () => {
+        shouldMove = true
+        myElement.style.cursor =  'move';
+    })
+    myElement.addEventListener('mouseup', () => {
+        shouldMove = false
+        myElement.style.cursor = 'pointer'
+    })
+
+    let top = myElement.style.top ? parseInt(myElement.style.top) : 0
+    let left = myElement.style.left ? parseInt(myElement.style.left) : 0
+    if(shouldMove) {
+        myElement.style.top = `${top+(y - prevY)}px`
+        myElement.style.left = `${left+(x - prevX)}px`
+        myObj = myElement.style.cssText
+        console.log(myObj)
+    }
+    prevX = x
+    prevY = y
+
 })
 
-myElement.addEventListener('mouseup', (event)=> {
-    myElement.removeEventListener('mousemove', myEvent)
-    myElement.style.cursor = 'pointer'
-    console.log('unclick')
-    // mouseTrack(false);
-
-})
